dd9971e3d13cfb9ce57cf67e90edf7464b5f491f
AlMamunTalukder <almamuntalukder0@gmail.com>
2023-11-07 17:30:35 +0600

    view details

--- index.js
+++ index.js
@@ -2,16 +2,78 @@ const express = require("express");
 const cors = require("cors");
 var jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 //middelwares
 app.use(cors());
 app.use(express.json());
 //---------------------------
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zelauzs.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const repairServiceCollection = client
+      .db("repair-service")
+      .collection("services");
+
+    //Alls CRUD Operations
+    //get
+    app.get("/services", async (req, res) => {
+      const cursor = repairServiceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //for show details
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await repairServiceCollection.findOne(query);
+      res.send(result);
+      console.log(id);
+    });
+    // app.get("/services/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+
+    // const options = {
+    //   projection: { title: 1, price: 1, service_id: 1, img: 1 },
+    // }; // this is for finding specific data from db
+
+    //   const result = await serviceCollection.findOne(query, options);
+    //   res.send(result);
+    // });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//---------------------------
 app.get("/", (req, res) => {
-  res.send("Coffee making server is running");
+  res.send("Repair Service server is running");
 });
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Repair app listening on port ${port}`);
 });
